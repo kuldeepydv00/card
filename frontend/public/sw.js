bf6632a33f4fd9162b7c8fdcb8e1e797428074e2
kuldeepydv00 <kuldeepyadav200507@gmail.com>
2026-06-27 15:35:04 +0530

    Update service worker to clear cache and use png icon

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,15 +1,29 @@
-const CACHE_NAME = '50xcards-v1';
+const CACHE_NAME = '50xcards-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon.svg'
+  '/icon.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
